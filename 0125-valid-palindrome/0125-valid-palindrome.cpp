56163dc2b46a622b64c91a4973851e95f9088437
Anish Sahu <134163703+anishsahu10@users.noreply.github.com>
2024-05-17 01:13:54 +0530

    Time: 3 ms (90.30%), Space: 8.7 MB (61.42%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0;
+        int right = s.size()-1;
+        
+        for(int i = 0; i< s.size(); i++){
+            s[i] = tolower(s[i]);
+        }
+        
+        while(left<=right){
+            if(!isalnum(s[left])){
+                left++;
+                continue;
+            }
+            if(!isalnum(s[right])){
+                right--;
+                continue;
+            }
+            if(s[left] != s[right]){
+                return false;
+            }
+            else{
+                left++;
+                right--;
+            }
+            
+        }
+        return true;
+    }
+};
