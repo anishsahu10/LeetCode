ba83c4200f47522a2b78b9e1cefbfc30a287b1ee
Anish Sahu <134163703+anishsahu10@users.noreply.github.com>
2024-05-31 13:03:38 +0530

    Time: 2 ms (51.98%), Space: 9.1 MB (72.44%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int countSA(vector<int>& nums, int maxSum){
+        int subArr = 1, sum = 0;
+        for(int i = 0; i<nums.size(); i++){
+            if(sum + nums[i]<=maxSum) sum += nums[i];
+            else{
+                subArr++;
+                sum = nums[i];
+            }
+        }
+        return subArr;
+    }
+    
+    
+    int splitArray(vector<int>& nums, int k) {
+        if(k>nums.size()) return -1;
+        int ans;
+        int l = *max_element(nums.begin(), nums.end());
+        int h = accumulate(nums.begin(), nums.end(), 0);
+        
+        while(l<=h){
+            int m = (l+h)/2;
+            int noOfSA = countSA(nums, m);
+            if(noOfSA > k) l = m+1;
+            else h = m-1;
+        }
+        return l;
+    }
+};
