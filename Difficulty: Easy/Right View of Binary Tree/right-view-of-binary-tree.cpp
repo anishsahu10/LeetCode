eb3822da7712c36963d3aebb578e079b09acb85b
Anish Sahu <134163703+anishsahu10@users.noreply.github.com>
2024-06-28 15:30:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Right View of Binary Tree/right-view-of-binary-tree.cpp
+++ Difficulty: Easy/Right View of Binary Tree/right-view-of-binary-tree.cpp
@@ -49,12 +49,12 @@ class Solution
         q.push(root);
         while(!q.empty()){
             int n = q.size();
-            ans.push_back(q.back()->data);
+            ans.push_back(q.front()->data);
             for(int i = 0; i<n; i++){
                 Node* temp  = q.front();
                 q.pop();
-                if(temp->left) q.push(temp->left);
                 if(temp->right) q.push(temp->right);
+                if(temp->left) q.push(temp->left);
             }
         }
         return ans;
